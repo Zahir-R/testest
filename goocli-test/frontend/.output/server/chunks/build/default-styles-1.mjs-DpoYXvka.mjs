4101b71eb32950fc8c534aaaaa0025c38fef43c5
Zahir-R <zahiriv4@gmail.com>
2026-03-07 22:43:02 -0400

    Change baseDirectory

--- goocli-test/frontend/.output/server/chunks/build/default-styles-1.mjs-DpoYXvka.mjs
+++
@@ -1,4 +0,0 @@
-const default_vue_vue_type_style_index_0_lang = "html{font-size:18px}body{line-height:1.6}";
-
-export { default_vue_vue_type_style_index_0_lang as d };
-//# sourceMappingURL=default-styles-1.mjs-DpoYXvka.mjs.map
--- goocli-test/frontend/.output/server/chunks/build/default-styles.DwPSLcMr.mjs
+++
@@ -1,6 +0,0 @@
-import { d as default_vue_vue_type_style_index_0_lang } from './default-styles-1.mjs-DpoYXvka.mjs';
-
-const defaultStyles_DwPSLcMr = [default_vue_vue_type_style_index_0_lang, default_vue_vue_type_style_index_0_lang];
-
-export { defaultStyles_DwPSLcMr as default };
-//# sourceMappingURL=default-styles.DwPSLcMr.mjs.map
